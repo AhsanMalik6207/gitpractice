12409d5c4d379554f8318f1239cb50753b84778a
zubair4241 <zubairrajpoot4241@gmail.com>
2022-03-30 18:28:54 +0500

    zain
    Zubair

--- src/container/Header.js
+++ src/container/Header.js
@@ -1,9 +1,15 @@
 import React from "react";
+import "./header.css"
 const Header = () => {
     return (
       <div className="ui fixed menu">
         <div className="ui container center">
-          <h2>FakeShop</h2>
+          <ul className='navbar'>
+           <li> Ecommerce Shop </li>
+           <li> Home </li>
+           <li> Contact us</li>
+           <li> Gallery </li>
+            </ul>
         </div>
       </div>
     );
