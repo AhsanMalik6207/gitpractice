2b30be03310ee57f128702a977bf7bf660cb1b84
Mohsin <mohsin.ali@invozone.com>
2022-03-31 16:07:46 +0500

    my-comit

--- src/container/Footer.js
+++ src/container/Footer.js
@@ -1,16 +1,24 @@
-import { AppBar, Toolbar, Typography, Container } from "@material-ui/core";
-import '../App.css'
+import { AppBar, Toolbar, Typography, Container } from '@material-ui/core';
+import '../App.css';
 
 export default function Footer() {
-    return (
-        <AppBar position="static" color="white" className="footer">
-          <Container maxWidth="md">
-            <Toolbar>
-              <Typography variant="h5" color="black">
-                 @ Shopping-Store @ 2022
-              </Typography>
-            </Toolbar>
-          </Container>
-        </AppBar>
-    )
+	return (
+		<AppBar
+			style={{ backgroundColor: 'gray' }}
+			position="static"
+			color="white"
+			className="footer"
+		>
+			<Container maxWidth="md">
+				<Toolbar>
+					<Typography variant="h5" color="black">
+						<div>
+							<h2>Mohsin@gmail.com</h2>
+							<h2>@ Shopping-Store @ 2022</h2>
+						</div>
+					</Typography>
+				</Toolbar>
+			</Container>
+		</AppBar>
+	);
 }
