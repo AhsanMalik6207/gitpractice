6dd04920b365b515d3d6df4d93f33c41a8fe7a20
Mohsin <mohsin.ali@invozone.com>
2022-03-31 11:19:47 +0500

    designing changes

--- src/container/Footer.js
+++ src/container/Footer.js
@@ -1,15 +1,19 @@
-import { AppBar, Toolbar, Typography, Container } from "@material-ui/core";
+import { AppBar, Toolbar, Typography, Container } from '@material-ui/core';
 
 export default function Footer() {
-    return (
-        <AppBar position="static" color="white">
-          <Container maxWidth="md">
-            <Toolbar>
-              <Typography variant="h6" color="black">
-                &copy; 2022 @Ahsan Shopping-Store
-              </Typography>
-            </Toolbar>
-          </Container>
-        </AppBar>
-    )
+	return (
+		<AppBar
+			position="static"
+			color="blue"
+			style={{ backgroundColor: '#6d6a6a', color: 'blue' }}
+		>
+			<Container maxWidth="sm" background-color="#6d6a6a">
+				<Toolbar>
+					<Typography variant="h6" color="blue">
+						&copy; 2022 @Ahsan Shopping-Store
+					</Typography>
+				</Toolbar>
+			</Container>
+		</AppBar>
+	);
 }
