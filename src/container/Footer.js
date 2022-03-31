767822e56ad607c303f7b888e67fb4ddcce06dd0
Invozone <ahsanmalik6229@gmail.com>
2022-03-31 16:23:58 +0500

    done

--- src/container/Footer.js
+++ src/container/Footer.js
@@ -1,26 +1,9 @@
 import { AppBar, Toolbar, Typography, Container } from '@material-ui/core';
-<<<<<<< HEAD
 import '../App.css';
-=======
->>>>>>> origin/mohsin
 
 export default function Footer() {
 	return (
 		<AppBar
-<<<<<<< HEAD
-			style={{ backgroundColor: 'gray' }}
-			position="static"
-			color="white"
-			className="footer"
-		>
-			<Container maxWidth="md">
-				<Toolbar>
-					<Typography variant="h5" color="black">
-						<div>
-							<h2>Mohsin@gmail.com</h2>
-							<h2>@ Shopping-Store @ 2022</h2>
-						</div>
-=======
 			position="static"
 			color="blue"
 			style={{ backgroundColor: '#6d6a6a', color: 'blue' }}
@@ -29,7 +12,6 @@ export default function Footer() {
 				<Toolbar>
 					<Typography variant="h6" color="blue">
 						<h2>&copy; 2022 @Ahsan Shopping-Store</h2>
->>>>>>> origin/mohsin
 					</Typography>
 				</Toolbar>
 			</Container>
